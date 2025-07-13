73f9147d817368c67d18ac41975620b2186a87a1
Nitin <nitinparjapat94@gail.com>
2025-07-13 16:26:26 +0530

    Adding Backend func inc  databases

--- exp_app/models.py
+++ exp_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class current_ammount(models.Model):
+    current_balance = models.FloatField(default=0)
+class Tracker(models.Model):
+    current_balance = models.ForeignKey(current_ammount, on_delete=models.CASCADE)
+    expense_type = models.CharField(max_length=100,choices=(("Credit","Credit"),("Debit","Debit")))
+    ammount = models.FloatField(default=0)
+    description = models.CharField(max_length=1000)
+    created_at = models.DateField(auto_now = True)
+    updated_at = models.DateField(auto_now_add=True)
--- exp_app/views.py
+++ exp_app/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from .models import Tracker,current_ammount
 # Create your views here.
+
+def home(request):
+    if request.method == "POST":
+        description = request.POST.get('description')
+        ammount = request.POST.get('ammount')
+        current_balance, _ = current_ammount.objects.get_or_create(id = 1)
+        expense_type = "Credit"
+        if float(ammount) < 0:
+            expense_type = "Debit"
+        tracking = Tracker.objects.create(
+        ammount=ammount,expense_type=expense_type,
+        current_balance=current_balance,
+        description=description)
+        current_balance.current_balance += float(tracking.ammount)
+        current_balance.save()
+        return redirect('/')
+    return render(request, 'base.html')
