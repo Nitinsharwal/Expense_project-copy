10fa441da948d062cd76068247a21a0c73c26ef8
Nitin <nitinparjapat94@gail.com>
2025-08-12 16:26:10 +0530

    New

--- expense_prj/settings.py
+++ expense_prj/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-f@9ltu%n8%7p^x1hhyaj+ve36h2=_xat&v5cytq94d0i2j%0a_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.vercel.app','127.0.0.1','.now.sh']
 
